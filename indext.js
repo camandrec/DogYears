13c59c802a42f53c0eaba8ba80ab63d02d3fca98
Camilo Recio <camilorecio@gmail.com>
2020-05-03 10:04:48 -0500

    First Commit

---
+++ indext.js
@@ -0,0 +1,12 @@
+let myAge = 27; //Definition of the variable that holds my age in years.
+let earlyYears = 2; //Definitions of the variable that holds the durationg of early years of a dog.
+earlyYears *= 10.5; //Product of the early years by the age each of them accounts for.
+let laterYears = myAge - 2;
+laterYears *= 4; //Calculate the age for the rest of years.
+myAgeInDogYears = earlyYears + laterYears; //Calculates my age in dog years.
+let myName = "Cam".toLowerCase(); //Method to lowercase my name.
+
+console.log(earlyYears)
+console.log(laterYears)
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`) // This statement displays my name, age and age in dog years.
